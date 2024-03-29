07f39fbb712845ee7be1bba7be2683d44cc502d2
leoj2eephp <leandro.j2ee@gmail.com>
2024-03-29 13:40:36 -0300

    New. Se agrega API para confirmar/rechazar asignación.

--- modules/v1/controllers/AsignacionController.php
+++ modules/v1/controllers/AsignacionController.php
@@ -8,6 +8,7 @@ use Yii;
 use yii\data\ActiveDataProvider;
 use yii\filters\VerbFilter;
 use yii\rest\ActiveController;
+use yii\web\Response;
 
 /**
  * Default controller for the `v1` module
@@ -21,12 +22,24 @@ class AsignacionController extends ActiveController {
      * @return string
      */
     public function actionMisAsignaciones() {
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        Yii::$app->response->format = Response::FORMAT_JSON;
         $postData = file_get_contents('php://input');
         $data = json_decode($postData);
         $asignaciones = Asignacion::find()->with(["userId1", "userId2"])
-                        ->where("user_id1 = :uid OR user_id2 = :uid", [":uid" => $data->id])->all();
+            ->where("user_id1 = :uid OR user_id2 = :uid", [":uid" => $data->id])->all();
         return $asignaciones;
+    }
+
+    public function actionConfirmReject() {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $postData = file_get_contents('php://input');
+        $data = json_decode($postData);
+        $asignacion = Asignacion::find()->where("id = :id", [":id" => $data->idAsignacion])->one();
+        if ($data->confirm1 != null) $asignacion->confirmado1 = (int) $data->confirm1;
+        if ($data->confirm2 != null) $asignacion->confirmado2 = (int) $data->confirm2;
+
+        if ($asignacion->save()) return true;
+        return join(",", $asignacion->firstErrors);
     }
 
     public function behaviors() {
@@ -35,7 +48,7 @@ class AsignacionController extends ActiveController {
             'class' => VerbFilter::class,
             'actions' => [
                 'mis-asignaciones' => ['post'],
-                'mis-asignaciones-dos' => ['post'],
+                'confirm-reject' => ['post'],
             ],
         ];
         return $behaviors;
