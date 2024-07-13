07360465628b4283bfb927c5d62db6b3eaec10e8
leoj2eephp <leandro.j2ee@gmail.com>
2024-07-12 22:51:37 -0400

    Fix. Admin permission for links.

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -5,8 +5,7 @@ use yii\helpers\Url;
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= Url::to("/site/index") ?>" class="brand-link">
-        <img src="<?= Yii::getAlias('@web') . '/images/logo.png' ?>" 
-            alt="PPAM" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= Yii::getAlias('@web') . '/images/logo.png' ?>" alt="PPAM" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">PPAM Osorno</span>
     </a>
 
@@ -19,10 +18,22 @@ use yii\helpers\Url;
                     ['label' => 'Inicio', 'url' => ['site/index'], 'icon' => 'home', 'visible' => !Yii::$app->user->isGuest],
                     ['label' => 'Administración', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Asignaciones', 'url' => ['asignacion/index'], 'icon' => 'calendar', 'visible' => Yii::$app->user->can("asignacion/index")],
-                    ['label' => 'Puntos', 'url' => ['punto/index'], 'icon' => 'map-pin', 'visible' => Yii::$app->user->can("punto/index")],
-                    ['label' => 'Turnos', 'url' => ['turno/index'], 'icon' => 'clock', 'visible' => Yii::$app->user->can("turno/index")],
-                    ['label' => 'Usuarios', 'url' => ['user/index'], 'icon' => 'user', 'visible' => Yii::$app->user->can("user/index")],
+                    [
+                        'label' => 'Asignaciones', 'url' => ['asignacion/index'], 'icon' => 'calendar',
+                        'visible' => Yii::$app->user->can("asignacion/index") || Yii::$app->user->can("admin")
+                    ],
+                    [
+                        'label' => 'Puntos', 'url' => ['punto/index'], 'icon' => 'map-pin',
+                        'visible' => Yii::$app->user->can("punto/index") || Yii::$app->user->can("admin")
+                    ],
+                    [
+                        'label' => 'Turnos', 'url' => ['turno/index'], 'icon' => 'clock',
+                        'visible' => Yii::$app->user->can("turno/index") || Yii::$app->user->can("admin")
+                    ],
+                    [
+                        'label' => 'Usuarios', 'url' => ['user/index'], 'icon' => 'user',
+                        'visible' => Yii::$app->user->can("user/index") || Yii::$app->user->can("admin")
+                    ],
                     [
                         'label' => 'Disponibilidad', 'url' => ['disponibilidad/update', "id" => Yii::$app->user->id], 'icon' => 'check',
                         'visible' => Yii::$app->user->can("disponibilidad/update") && !Yii::$app->user->can("admin")
