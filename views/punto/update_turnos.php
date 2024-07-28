bd9f46d2cef99328187704a5d0ba7aefd33a9d33
leoj2eephp <leandro.j2ee@gmail.com>
2024-07-28 17:20:40 -0400

    sdf

--- views/punto/update_turnos.php
+++ views/punto/update_turnos.php
@@ -111,7 +111,7 @@ $script = <<< JS
                 body: JSON.stringify(data),
                 headers: {
                     "Content-Type": "application/json",
-                    "X-CSRF-Token": "<?= Yii::$app->request->getCsrfToken() ?>"
+                    // "X-CSRF-Token": "<?= Yii::$app->request->getCsrfToken() ?>"
                 },
             })
             .then((res) => res.json())
